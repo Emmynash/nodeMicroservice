53ea60a5c4eef89d7f786261632c1b0c483687cc
E Akita <emmy4nash@gmail.com>
2019-02-26 07:05:02 +0100

    update documentation

--- server/server.js
+++ server/server.js
@@ -10,52 +10,66 @@ const { authenticate } = require('./middleware/authenticate');
 const app = express();
 const port = process.env.PORT || 8080;
 
-
+// middleware
 app.use(bodyParser.json());
 
+// POST /uer/login route (Login valid user)
 app.post('/user/login', (req, res) => {
 
     try {
+        // pick email & password set by user
         let body = _.pick(req.body, ['email', 'password'])
 
+        // validate email & password
         if (!validator.isEmail(body.email)) {
             return res.status(400).send(`${body.email} does not exist`);
         } else if (body.password.length == 0) {
             return res.status(400).send(`Password is required!`);
         }
 
+        // create a jsonwebtoken
         let userId = 00247;
         let token = jwt.sign({ _id: userId, access: "auth" }, "tokenfy").toString();
 
-        res.header('x-auth', token).send(token);
+        // set 'x-auth' header with the token value and return the token
+        res.header('x-auth', token).json({ success: true, token });
 
     } catch (error) {
-        res.status(400).send();
+        res.status(400).send({ success: false });
     }
 
 })
 
+// PATCH user/patch route (patch user document)
 app.patch('/user/patch', authenticate, async(req, res) => {
     try {
+        // pick age & sex values set by user
         let body = _.pick(req.body, ['age', 'sex'])
+
         let userDoc = { "age": "0", "sex": "undefine", "hobby": "Reading" };
+
+        // update user document by setting new values for sex & age
         let patch = [
             { "op": "replace", "path": "/age", "value": body.age },
             { "op": "replace", "path": "/sex", "value": body.sex }
         ]
 
+        // Patch the documents and return it to user
         let patchUserDoc = await jsonpath.apply(userDoc, patch);
         res.send(patchUserDoc);
 
     } catch (error) {
-        res.status(400).send();
+        res.status(400).send({ success: false });
     }
 })
 
+// POST user/me/image (download and return image)
 app.post('/user/me/image', authenticate, (req, res) => {
 
+    // pick the image url
     let body = _.pick(req.body, ['url']);
 
+    // download the image and return it to user
     download.image({
             url: body.url,
             dest: "./public/images"
@@ -66,7 +80,7 @@ app.post('/user/me/image', authenticate, (req, res) => {
             return image;
         })
         .catch((err) => {
-            res.status(400).send(err)
+            res.status(400).send({ success: false });
         })
 
 })
